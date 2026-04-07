2ef6960463714b6e3f8567c99b581bd784a01f7b
unknown <pjhh060328@gmail.com>
2026-04-07 16:03:27 +0900

    [0407] 카운트다운 페이지

---
+++ countdown/countdown.js
@@ -0,0 +1,43 @@
+function updateTimer() {
+    const now = new Date();
+    
+    const openTime = new Date();
+    openTime.setHours(7, 0, 0, 0); 
+    
+    const closeTime = new Date();
+    closeTime.setHours(23, 0, 0, 0);
+
+    const statusMsg = document.querySelector('.description');
+    const timerDisplay = document.querySelector('.timer');
+
+    let diff; 
+
+    if (now < openTime) {
+        // 1. 운영 전
+        diff = openTime - now;
+        statusMsg.innerText = "금일 오픈까지 남은 시간";
+        timerDisplay.innerText = formatTime(diff);
+    } 
+    else if (now >= openTime && now < closeTime) {
+        // 2. 운영 중
+        diff = closeTime - now;
+        statusMsg.innerText = "금일 마감까지 남은 시간";
+        timerDisplay.innerText = formatTime(diff);
+    } 
+    else {
+        // 3. 마감 후
+        statusMsg.innerText = "금일 마감";
+        timerDisplay.innerText = "";
+    }
+}
+
+
+function formatTime(ms) {
+    let h = Math.floor((ms / (1000 * 60 * 60)) % 24);
+    let m = Math.floor((ms / (1000 * 60)) % 60);
+    let s = Math.floor((ms / 1000) % 60);
+    
+    return `${h}:${m}:${s}`;
+}
+
+setInterval(updateTimer, 1000);
